f68ae52b00f259eadce0abfb6bdee2bdafe0a9f7
Moritz Bosse <moritz.bosse@destatis.de>
2026-07-28 14:23:57 +0200

    1.1

--- _includes/assets/js/view/chartHelpers.js
+++ _includes/assets/js/view/chartHelpers.js
@@ -129,7 +129,7 @@ function updateIndicatorDataSeriesStatus(series) {
     //return isHighContrast(contrast) ? '{{ site.graph_color_headline_high_contrast | default: "#FFDD00" }}' : '{{ site.graph_color_headline | default: "#00006a" }}';
 function getHeadlineColor(contrast, goalNumber) {
 
-  var headlineColors = ["#e5243b", "#dda63a", "#4c9f38", "#c5192d", "#ff3a21", "#26bde2", "#fcc30b", "#a21942", "#fd6925", "#dd1367", "#fd9d24", "#bf8b2e", "#3f7e44", "#0a97d9", "#56c02b", "#00689d", "#19486a"];
+  var headlineColors = ['#AA2B5F', '#FCB513', '#9E5EA6', '#71BE43'];
   var headlineColor = headlineColors[goalNumber-1];
   var htmlString = '{{ "' + headlineColor + '" | default: "#00006a" }}';
   console.log("goalNumber: ", htmlString);
