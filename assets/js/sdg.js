c38fa22236c56544fadd7325d74210308715fbf6
MoBosse <50657629+MoBosse@users.noreply.github.com>
2026-07-28 12:26:31 +0000

    Deploying to gh-pages from  @ f68ae52b00f259eadce0abfb6bdee2bdafe0a9f7 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3855,7 +3855,7 @@ function updateIndicatorDataSeriesStatus(series) {
     //return isHighContrast(contrast) ? '#FFDD00' : '#b8b8b8';
 function getHeadlineColor(contrast, goalNumber) {
 
-  var headlineColors = ["#e5243b", "#dda63a", "#4c9f38", "#c5192d", "#ff3a21", "#26bde2", "#fcc30b", "#a21942", "#fd6925", "#dd1367", "#fd9d24", "#bf8b2e", "#3f7e44", "#0a97d9", "#56c02b", "#00689d", "#19486a"];
+  var headlineColors = ['#AA2B5F', '#FCB513', '#9E5EA6', '#71BE43'];
   var headlineColor = headlineColors[goalNumber-1];
   var htmlString = '' + headlineColor + '';
   console.log("goalNumber: ", htmlString);
